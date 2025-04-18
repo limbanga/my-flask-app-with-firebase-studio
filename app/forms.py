9bf1287e8e1b8f5969ac14e180e7faabf8bb3a04
thliem143 <thliem143@gmail.com>
2025-04-18 02:52:15 +0000

    add create update movie feature

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import (DateField, IntegerField, PasswordField, StringField,
+                     SubmitField)
+from wtforms.validators import (URL, DataRequired, Email, EqualTo, Length,
+                                Optional)
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -13,3 +16,15 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Mật khẩu', validators=[DataRequired()])
     confirm_password = PasswordField('Xác nhận mật khẩu', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Đăng ký')
+
+
+
+class MovieForm(FlaskForm):
+    title = StringField('Tiêu đề', validators=[DataRequired()])
+    genre = StringField('Thể loại', validators=[Optional()])
+    duration = IntegerField('Thời lượng (phút)', validators=[Optional()])
+    director = StringField('Đạo diễn', validators=[Optional()])
+    poster_url = StringField('URL poster', validators=[Optional(), URL()])
+    release_date = DateField('Ngày khởi chiếu', validators=[Optional()])
+    rated = StringField('Độ tuổi', validators=[Optional()])
+    submit = SubmitField('Lưu')
--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from app.models.movie import Movie
+from app.forms import MovieForm
+from app import db
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -14,3 +16,39 @@ def dashboard():
 def manage_movies():
     movies = Movie.query.all()
     return render_template('admin/movies.html', movies=movies)
+
+# Thêm phim
+@admin_bp.route('/movies/create', methods=['GET', 'POST'])
+@login_required
+def create_movie():
+    form = MovieForm()
+    if form.validate_on_submit():
+        movie = Movie(
+            title=form.title.data,
+            genre=form.genre.data,
+            duration=form.duration.data,
+            director=form.director.data,
+            poster_url=form.poster_url.data,
+            release_date=form.release_date.data,
+            rated=form.rated.data
+        )
+        db.session.add(movie)
+        db.session.commit()
+        flash('🎉 Thêm phim thành công!')
+        return redirect(url_for('admin.manage_movies'))
+    else: 
+        print(form.errors)
+    return render_template('admin/create_movie.html', form=form)
+
+# Chỉnh sửa phim
+@admin_bp.route('/movies/<int:movie_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_movie(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    form = MovieForm(obj=movie)
+    if form.validate_on_submit():
+        form.populate_obj(movie)
+        db.session.commit()
+        flash('✅ Cập nhật phim thành công!')
+        return redirect(url_for('admin.manage_movies'))
+    return render_template('admin/create_movie.html', form=form, movie=movie)
