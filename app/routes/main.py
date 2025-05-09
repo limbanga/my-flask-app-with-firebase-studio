3c629f2c6c44533a0de7e0e75a0e25d71c2c9e4b
limbanga <95570833+limbanga@users.noreply.github.com>
2025-05-09 22:47:43 +0700

    feat: add cinema card component and update theaters section for dynamic cinema display

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,24 +1,17 @@
 from flask import Blueprint, render_template
 from app.models.movie import Movie
+from app.models.cinema import Cinema
 
 import logging
 
-logger = logging.getLogger('flask_app') 
+logger = logging.getLogger("flask_app")
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
-@main_bp.route('/')
+
+@main_bp.route("/")
 def index():
-    """
-    Trang chủ của website chiếu phim.
-
-    - Phương thức: GET
-    - Mục đích: Hiển thị danh sách tất cả các bộ phim hiện có trong hệ thống.
-    - Cơ chế:
-        + Truy vấn tất cả các bộ phim từ cơ sở dữ liệu.
-        + Truyền dữ liệu phim vào template 'main/index.html' để hiển thị.
-    - Trả về: HTML hiển thị giao diện trang chủ với danh sách phim.
-    """
     logger.info("User accessed profile page")
+    cinemas = Cinema.query.all()
     movies = Movie.query.all()
-    return render_template('main/index.html', movies=movies)
+    return render_template("main/index.html", movies=movies, cinemas=cinemas)
